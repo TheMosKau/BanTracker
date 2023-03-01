5bc59d8d07e7765f61d4e10b23cf64ffbd6d5caf
charlotte <71281295+chaarlottte@users.noreply.github.com>
2023-02-28 22:12:21 -0500

    you didnt actually need to gather cookies

--- bot.py
+++ bot.py
@@ -3,10 +3,6 @@ import time
 import json
 import requests
 from colorama import Fore
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import discord, time
 from discord.ext import tasks
 from discord.ext import commands
@@ -25,38 +21,24 @@ session = requests.Session()
 owd_bans = None
 ostaff_bans = None
 
-def getCookies():
-    chromedriver_path = "chromedriver.exe"
-    browser = webdriver.Chrome(chromedriver_path)
-    browser.get("https://plancke.io")
-    wait = WebDriverWait(browser, 15)
-    wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id=\"wrapper\"]/div[1]/div[2]/div")))
-    time.sleep(5)
-    cookies = {}
-    for cookie in browser.get_cookies():
-        cookies[cookie["name"]] = cookie["value"]
-    browser.quit()
-    print(cookies)
-    session.headers.update({
-        "Accept": "application/json",
-        "Origin": "https://plancke.io",
-        "Referer": "https://plancke.io/",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
-    })
-    session.cookies.update(cookies)
+session.headers.update({
+    "Accept": "application/json",
+    "Origin": "https://plancke.io",
+    "Referer": "https://plancke.io/",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+})
 
 #Startup
 @bot.event
 async def on_ready():
     global channel
-    getCookies()
     channel = bot.get_channel(channel_id)
     print(f"Logging channel set to {channel.name}")
     checkloop.start()
     print(f"{Fore.LIGHTGREEN_EX}Checker has started!{Fore.RESET}")
 
 # The actual checker
-@tasks.loop(seconds=1)
+@tasks.loop(seconds=0.1)
 async def checkloop():
     global owd_bans, ostaff_bans, session
     resp = session.get(
